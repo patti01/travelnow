26c4f8ba51a18837db5ffa8966b6b2f31ca23728
Patricia Falandysz <pfalandysz@yahoo.com>
2019-06-24 16:49:33 +0200

    transportation scraping done

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_24_075114) do
+ActiveRecord::Schema.define(version: 2019_06_24_135153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "destinations", force: :cascade do |t|
     t.string "destination_name"
-    t.integer "transport_price"
+    t.string "transport_price"
     t.string "photo_link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,56 +1,71 @@
+require 'open-uri'
+require 'nokogiri'
+
 Destination.destroy_all
 
 puts "Destroying old data..."
 
 puts "Creating new seed..."
 
+def scraper(destination_name)
+  html_file = open("https://www.numbeo.com/cost-of-living/in/#{destination_name}").read
+  html_doc = Nokogiri::HTML(html_file)
+
+
+  html_doc.search('.data_wide_table tr:nth-of-type(31) .priceValue').each do |element|
+    transport_price = element.text.strip
+  end
+end
+
 destination1= Destination.create(
 destination_name: "Madrid",
-transport_price: 3,
-photo_link: "https://images.kiwi.com/photos/600x600/madrid_es.jpg"
+photo_link: "https://images.kiwi.com/photos/600x600/madrid_es.jpg",
+transport_price: scraper("Madrid").text.strip,
 )
+
 
 destination2= Destination.create(
 destination_name: "Barcelone",
-transport_price: 3,
+transport_price: scraper("Barcelona").text.strip,
 photo_link: "https://images.kiwi.com/photos/600x600/barcelona_es.jpg"
 )
 
 destination3= Destination.create(
 destination_name: "Porto",
-transport_price: 3,
+transport_price: scraper("Porto").text.strip,
 photo_link: "https://images.kiwi.com/photos/600x600/porto_pt.jpg"
 )
 
 destination4= Destination.create(
 destination_name: "Venise",
-transport_price: 3,
+transport_price: scraper("Venice").text.strip,
 photo_link: "https://images.kiwi.com/photos/600x600/venice_it.jpg"
 )
 
 destination5= Destination.create(
 destination_name: "Berlin",
-transport_price: 3,
+transport_price: scraper("Berlin").text.strip,
 photo_link: "https://images.kiwi.com/photos/600x600/berlin_de.jpg"
 )
 
 destination6= Destination.create(
 destination_name: "Milan",
-transport_price: 3,
+transport_price: scraper("Milan").text.strip,
 photo_link: "https://images.kiwi.com/photos/600x600/milan_it.jpg"
 )
 
 destination7= Destination.create(
 destination_name: "Amsterdam",
-transport_price: 3,
+transport_price: scraper("Amsterdam").text.strip,
 photo_link: "https://images.kiwi.com/photos/600x600/amsterdam_nl.jpg"
 )
 
 destination8= Destination.create(
 destination_name: "Dublin",
-transport_price: 3,
+transport_price: scraper("Dublin").text.strip,
 photo_link: "https://images.kiwi.com/photos/600x600/dublin_ie.jpg"
 )
+
 
 puts "Seeded"
 
